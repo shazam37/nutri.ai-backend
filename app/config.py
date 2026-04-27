1a3364dfb500516214970588901f9387890ecbcb
shazam_arya <shaz.ajmal@arya.ai>
2026-04-28 01:27:01 +0530

    minor: Update database configuration and dependencies

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     GROQ_API_KEY: str
     USDA_API_KEY: str
-    DATABASE_URL: str = "postgresql+asyncpg://nutriai@127.0.0.1:5432/nutriai"
+    DATABASE_URL: str 
     JWT_SECRET: str 
     
     class Config:
